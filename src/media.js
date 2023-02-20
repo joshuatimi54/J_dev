a55a626af6c7707ddebfe4250b0ebf73277293be
joshuatimi54 <105829140+joshuatimi54@users.noreply.github.com>
2023-02-20 13:58:36 +0100

    new

--- src/media.js
+++ src/media.js
@@ -5,24 +5,24 @@ const Content = () => {
             <div id="Work" className="name">Adesoba</div>
             <div className="site text-center">
                 <ul>
-                    <li><a href="https://dribbble.com/Joshua_timi"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+                    <li><a href="http://www.linkedin.com/in/joshua-adesoba-9144bb232"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
                     <li>
                         <a href="http://">
                             <i class="fa fa-twitter" aria-hidden="true"></i>
                         </a>
                     </li>
                     <li>
-                        <a href="https://github.com/joshuatimi54" target="_blank">
+                        <a href="https://github.com/joshuatimi54">
                             <i class="fa fa-github" aria-hidden="true"></i>
                         </a>
                     </li>
                     <li>
-                        <a href="http://www.linkedin.com/in/joshua-adesoba-9144bb232" target="_blank">
+                        <a href="http://www.linkedin.com/in/joshua-adesoba-9144bb232">
                             <i class="fa fa-linkedin-square" aria-hidden="true"></i>
                         </a>
                     </li>
                     <li>
-                        <a href="https://wa.me/message/TBTYBWC3YLNUK1" target="_blank">
+                        <a href="https://wa.me/message/TBTYBWC3YLNUK1">
                             <i class="fa fa-whatsapp" aria-hidden="true"></i>
                         </a>
                     </li>
